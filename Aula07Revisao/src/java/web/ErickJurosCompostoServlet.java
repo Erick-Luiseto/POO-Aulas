9b9463f7e954784f0fa52bf523229912aafb302c
Erick Luiseto <103527165+Erick-Luiseto@users.noreply.github.com>
2022-10-03 21:08:55 -0300

    AULA 07 - REVISÃO

---
+++ Aula07Revisao/src/java/web/ErickJurosCompostoServlet.java
@@ -0,0 +1,97 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.json.JSONObject;
+
+/**
+ *
+ * @author Fatec
+ */
+@WebServlet(name = "ErickJurosCompostoServlet", urlPatterns = {"/erick-juros-composto.json"})
+public class ErickJurosCompostoServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/ html;charset=UTF-8");
+        try ( PrintWriter out = response.getWriter()) {
+
+            out.println("<h4><a href='index.html'>VOLTAR</a></h4>");
+            double c= 0, i = 0, t = 0, r=0, mt = 0;
+            try{
+            c = Double.parseDouble(request.getParameter("n4"));
+            i = Double.parseDouble(request.getParameter("n5"));
+            t = Double.parseDouble(request.getParameter("n6"));
+            r = (c*Math.pow(1+(i/100), t) - c);
+            mt = r + c;
+            }catch(Exception ex){
+                out.println("<div style='color:red'> "
+                        + "Parametro Invalido"
+                        + "</div>");
+            }
+            JSONObject jcomposto = new JSONObject();
+            jcomposto.put("Valor do Juros Composto: ", r);
+            jcomposto.put("Valor do Montante: : ", mt);
+            response.getWriter().println(jcomposto.toString());
+       
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
